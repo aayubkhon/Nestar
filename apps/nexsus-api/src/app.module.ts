37948d9c3f5f27818c8c55106854026a5267a618
aayubxon701 <aayubxon701@gmail.com>
2026-04-20 07:06:00 +0900

    feat: build project standards

--- apps/nexsus-api/src/app.module.ts
+++ apps/nexsus-api/src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { AppRessolver } from './app.resolver';
+import { ComponentsModule } from './components/components.module';
+import { DatabaseModule } from './database/database.module';
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
@@ -14,6 +16,8 @@ import { AppRessolver } from './app.resolver';
 			uploads: false,
 			autoSchemaFile: true,
 		}),
+		ComponentsModule,
+		DatabaseModule,
 	],
 	controllers: [AppController],
 	providers: [AppService, AppRessolver],
