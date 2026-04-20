b81eecbe2c405630b8ae6a559a383526ed04842d
aayubxon701 <aayubxon701@gmail.com>
2026-04-21 05:44:03 +0900

    feat: create member resolver & service model

--- apps/nexsus-api/src/components/member/member.module.ts
+++ apps/nexsus-api/src/components/member/member.module.ts
@@ -1,4 +1,31 @@
 import { Module } from '@nestjs/common';
+import { MemberResolver } from './member.resolver';
+import { MemberService } from './member.service';
+import { Mutation, Query } from '@nestjs/graphql';
 
-@Module({})
-export class MemberModule {}
+@Module({
+	providers: [MemberResolver, MemberService],
+})
+export class MemberModule {
+	constructor(private readonly memberService: MemberService) {}
+	@Mutation(() => String)
+	public async signup(): Promise<string> {
+		console.log('Mutation sgnup');
+		return this.memberService.signup();
+	}
+	@Mutation(() => String)
+	public async login(): Promise<string> {
+		console.log('Mutation login');
+		return this.memberService.login();
+	}
+	@Mutation(() => String)
+	public async updateMember(): Promise<string> {
+		console.log('Mutation updateMember');
+		return this.memberService.updateMember();
+	}
+	@Query(() => String)
+	public async getmember(): Promise<string> {
+		console.log('Mutation getmember');
+		return this.memberService.getmember();
+	}
+}
