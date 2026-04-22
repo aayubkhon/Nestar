f34d72973ad9a510acd5caf55678311a7ca879f5
aayubxon701 <aayubxon701@gmail.com>
2026-04-23 06:50:23 +0900

    feat: integrate validation pipes logic

--- apps/nexsus-api/src/app.module.ts
+++ apps/nexsus-api/src/app.module.ts
@@ -7,14 +7,16 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { AppRessolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
+import { ApolloServerPluginLandingPageLocalDefault as ApolloServer } from '@apollo/server/plugin/landingPage/default';
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
 		GraphQLModule.forRoot({
 			driver: ApolloDriver,
-			playground: true,
+			playground: false,
 			uploads: false,
 			autoSchemaFile: true,
+			plugins: [ApolloServer()],
 		}),
 		ComponentsModule,
 		DatabaseModule,
