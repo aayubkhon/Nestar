42ff2253d9177249a060aa3bb601a9de36279e41
aayubxon701 <aayubxon701@gmail.com>
2026-04-23 10:28:13 +0900

    fix: nexsus-batchdegi maints code lar nexsus-api ga ozgartirildi xato togrlandi

--- apps/nexsus-batch/src/main.ts
+++ apps/nexsus-batch/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { NexsusBatchModule } from './nexsus-batch.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NexsusBatchModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT_BATCH ?? 3000);
+	const app = await NestFactory.create(NexsusBatchModule);
+	await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
