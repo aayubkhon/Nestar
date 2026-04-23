1587646f34c6eb5c74712e1b10def1ee56c7df6a
aayubxon701 <aayubxon701@gmail.com>
2026-04-23 13:20:20 +0900

    fix: creation and verification jwt

--- apps/nexsus-api/src/components/member/member.service.ts
+++ apps/nexsus-api/src/components/member/member.service.ts
@@ -18,8 +18,7 @@ export class MemberService {
 		input.memberPassword = await this.authService.hashPassword(input.memberPassword);
 		try {
 			const result = await this.memberModel.create(input);
-			// TODO Authentication via TOKEN
-
+			result.accessToken = await this.authService.createToken(result);
 			return result;
 		} catch (err) {
 			console.log('Error,Service.model', err instanceof Error ? err.message : err);
@@ -41,6 +40,8 @@ export class MemberService {
 		}
 		const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword);
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+		response.accessToken = await this.authService.createToken(response);
+
 		return response;
 	}
 
