66ff6093e8c7f45b90f1da339b2f31b636370eec
aayubxon701 <aayubxon701@gmail.com>
2026-04-27 16:33:57 +0900

    feat: develop getAgents GraphQL API

--- apps/nexsus-api/src/components/member/member.resolver.ts
+++ apps/nexsus-api/src/components/member/member.resolver.ts
@@ -1,8 +1,8 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 import { InternalServerErrorException, UseGuards } from '@nestjs/common';
-import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
-import { Member } from '../../libs/dto/member/member';
+import { AgentsInquiry, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { Member, Members } from '../../libs/dto/member/member';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
@@ -55,7 +55,6 @@ export class MemberResolver {
 		return this.memberService.updateMember(memberId, input);
 	}
 
-	
 	@UseGuards(WithoutGuard)
 	@Query(() => Member)
 	public async getMember(@Args('memberId') input: string, @AuthMember('_id') memebrId: ObjectId): Promise<Member> {
@@ -64,8 +63,14 @@ export class MemberResolver {
 		return this.memberService.getMember(memebrId, targetId);
 	}
 
-	/** ADMIN **/
+	@UseGuards(WithoutGuard)
+	@Query(() => Members)
+	public async getAgents(@Args('input') input: AgentsInquiry, @AuthMember('_id') memberId: ObjectId): Promise<Members> {
+		console.log('Query: getAgents');
+		return this.memberService.getAgents(memberId, input);
+	}
 
+	/** ADMIN **/
 	@Roles(MemberType.ADMIN)
 	@UseGuards(RolesGuard)
 	@Mutation(() => String)
