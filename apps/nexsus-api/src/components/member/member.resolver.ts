beff202a90d88b02b009c95331458bdd6101c09a
aayubxon701 <aayubxon701@gmail.com>
2026-04-27 12:18:53 +0900

    fix: moify updayeMember GraphQL APIis

--- apps/nexsus-api/src/components/member/member.resolver.ts
+++ apps/nexsus-api/src/components/member/member.resolver.ts
@@ -9,6 +9,7 @@ import { ObjectId } from 'mongoose';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
 @Resolver()
 export class MemberResolver {
@@ -26,13 +27,6 @@ export class MemberResolver {
 	}
 
 	@UseGuards(AuthGuard)
-	@Mutation(() => String)
-	public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string> {
-		console.log('Mutation updateMember');
-		return this.memberService.updateMember();
-	}
-
-	@UseGuards(AuthGuard)
 	@Query(() => String)
 	public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
 		console.log('Query checkAuth');
@@ -46,6 +40,17 @@ export class MemberResolver {
 	public async checkAuthRoles(@AuthMember() authmember: Member): Promise<string> {
 		console.log('query:checkAuthRoles');
 		return `Hi ${authmember.memberNick}, you are ${authmember.memberType} (memberId: ${authmember._id})`;
+	}
+
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async updateMember(
+		@Args('input') input: MemberUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Member> {
+		console.log('Mutation updateMember');
+		delete input._id
+		return this.memberService.updateMember(memberId,input);
 	}
 
 	@Query(() => String)
