f78f5e0127b1289ef2577df5773d3891bac52e18
aayubxon701 <aayubxon701@gmail.com>
2026-05-04 01:51:39 +0900

    feat: configure  boardArticle module and intagrate boardArticle DTO logic

---
+++ apps/nexsus-api/src/components/board-article/board-article.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { BoardArticle } from '../../libs/dto/board-article/board-article';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class BoardArticleService {
+   constructor(@InjectModel('BoardArticle') private readonly boardArticle: Model<BoardArticle>) {}
+
+}
