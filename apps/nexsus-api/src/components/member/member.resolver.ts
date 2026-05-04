a6d1a1f07c663ef2235f2be7330da9f0a6220d95
aayubxon701 <aayubxon701@gmail.com>
2026-05-04 21:26:09 +0900

    feat: develop likeTargetMember GraphQL API logic

--- apps/nexsus-api/src/components/member/member.resolver.ts
+++ apps/nexsus-api/src/components/member/member.resolver.ts
@@ -72,6 +72,17 @@ export class MemberResolver {
 		return await this.memberService.getAgents(memberId, input);
 	}
 
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async likeTargetmember(
+		@Args('memberId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Member> {
+		console.log('Mutation: likeTargetmember');
+		const likeRefId = shapeIntoMongoObjectId(input)
+		return await this.memberService.likeTargetmember(memberId,likeRefId);
+	}
+
 	/** ADMIN **/
 	@Roles(MemberType.ADMIN)
 	@UseGuards(RolesGuard)
