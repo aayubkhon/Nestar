bcf7440f95eaf8627548ed4f2de0778c0430f121
aayubxon701 <aayubxon701@gmail.com>
2026-06-24 21:54:31 +0900

    feat: integration of lookupAuthmemberLiked complex query business logic

--- apps/nexsus-api/src/components/property/property.service.ts
+++ apps/nexsus-api/src/components/property/property.service.ts
@@ -16,7 +16,7 @@ import { PropertyStatus } from '../../libs/enums/property.enum';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
 import moment from 'moment';
-import { lookUpMember, shapeIntoMongoObjectId } from '../../libs/config';
+import { lookupAuthMemberLiked, lookUpMember, shapeIntoMongoObjectId } from '../../libs/config';
 import { LikeService } from '../like/like.service';
 import { LikeInput } from '../../libs/dto/like/like.input';
 import { LikeGroup } from '../../libs/enums/like.enum';
@@ -63,7 +63,7 @@ export class PropertyService {
 			const likeInput = { memberId: memberId, likeRefId: propertyId, likeGroup: LikeGroup.PROPERTY };
 			targetProperty.meLiked = await this.likeService.checkLikeExistence(likeInput);
 		}
-		
+
 		targetProperty.memberData = await this.memberService.getMember(null, targetProperty.memberId);
 		return targetProperty;
 	}
@@ -112,7 +112,7 @@ export class PropertyService {
 						list: [
 							{ $skip: (input.page - 1) * input.limit },
 							{ $limit: input.limit },
-							// meLiked
+							lookupAuthMemberLiked(memberId),
 							lookUpMember,
 							{ $unwind: '$memberData' },
 						],
