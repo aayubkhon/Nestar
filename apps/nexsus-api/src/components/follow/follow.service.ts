bcaa0a1a93d284096db4d25e54189601bd6aff09
aayubxon701 <aayubxon701@gmail.com>
2026-06-24 22:11:30 +0900

    fix: modify like interaction related logic

--- apps/nexsus-api/src/components/follow/follow.service.ts
+++ apps/nexsus-api/src/components/follow/follow.service.ts
@@ -6,7 +6,7 @@ import { MemberService } from '../member/member.service';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 import { T } from '../../libs/types/common';
-import { lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
 
 @Injectable()
 export class FollowService {
@@ -77,7 +77,7 @@ export class FollowService {
 						list: [
 							{ $skip: (page - 1) * limit },
 							{ $limit: limit },
-							// MeLiked
+							lookupAuthMemberLiked(memberId,"$followingId"),
 							// mefolowed
 							lookupFollowingData,
 							{ $unwind: '$followingData' },
@@ -108,7 +108,7 @@ export class FollowService {
 						list: [
 							{ $skip: (page - 1) * limit },
 							{ $limit: limit },
-							// MeLiked
+							lookupAuthMemberLiked(memberId,"$followerId"),
 							// mefolowed
 							lookupFollowerData,
 							{ $unwind: '$followerData' },
