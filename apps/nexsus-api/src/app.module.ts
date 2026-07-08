2de5f8723a84850b267739b5d352610e88e8acdd
aayubxon701 <aayubxon701@gmail.com>
2026-07-08 19:35:29 +0900

    feat: created WebsoCkot Connection process

--- apps/nexsus-api/src/app.module.ts
+++ apps/nexsus-api/src/app.module.ts
@@ -9,6 +9,7 @@ import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
 import { ApolloServerPluginLandingPageLocalDefault as ApolloServer } from '@apollo/server/plugin/landingPage/default';
 import { T } from './libs/types/common';
+import { SocketModule } from './socket/socket.module';
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
@@ -30,6 +31,7 @@ import { T } from './libs/types/common';
 		}),
 		ComponentsModule,
 		DatabaseModule,
+		SocketModule,
 	],
 	controllers: [AppController],
 	providers: [AppService, AppRessolver],
